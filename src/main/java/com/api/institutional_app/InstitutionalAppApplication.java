d068506efa0697794110473566b0e7eb7e2283a4
kevin-vinagre <lkevinvinagre@hotmail.com>
2026-03-30 09:38:10 -0400

    dotenv load configuration

--- src/main/java/com/api/institutional_app/InstitutionalAppApplication.java
+++ src/main/java/com/api/institutional_app/InstitutionalAppApplication.java
@@ -3,10 +3,13 @@ package com.api.institutional_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class InstitutionalAppApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
 		SpringApplication.run(InstitutionalAppApplication.class, args);
 	}
 
