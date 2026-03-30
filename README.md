b376a4f3d0a0accdb1e6b78b86ef119c38e6f9aa
kevin-vinagre <lkevinvinagre@hotmail.com>
2026-03-30 08:35:50 -0400

    First Commit test

---
+++ README.md
@@ -0,0 +1,1 @@
+#FairTech Vote
---
+++ src/main/java/com/api/institutional_app/InstitutionalAppApplication.java
@@ -0,0 +1,13 @@
+package com.api.institutional_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstitutionalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstitutionalAppApplication.class, args);
+	}
+
+}
