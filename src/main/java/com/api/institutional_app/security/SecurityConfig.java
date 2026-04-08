af89beb5eed7673b1e0a69e514c724d0daeef106
kevin-vinagre <lkevinvinagre@hotmail.com>
2026-04-08 09:56:28 -0400

    jwt configuration

--- src/main/java/com/api/institutional_app/security/SecurityConfig.java
+++ src/main/java/com/api/institutional_app/security/SecurityConfig.java
@@ -4,14 +4,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/institutes/**").permitAll()
                         .requestMatchers("/audit/**").permitAll()
